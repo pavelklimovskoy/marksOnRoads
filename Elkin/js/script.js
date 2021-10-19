07e9f57e37d74a5c633bcbdd592f1f1cb9fb47fc
DmitryDichenskov <86311960+DmitryDichenskov@users.noreply.github.com>
2021-10-19 19:13:14 +0300

    First commit

---
+++ Elkin/js/script.js
@@ -0,0 +1,15 @@
+function onLoadButton() {
+    alert("Кнопка загрузки")
+  }
+  function onSaveButton() {
+    alert("Кнопка сохранения")
+  }
+  function onAgentButton() {
+    alert("Кнопка добаввления агента")
+  }
+  function onRoadButton() {
+    alert("Кнопка добавления дороги")
+  }
+  function onExecuteButton() {
+    alert("Кнопка вывода файла")
+  }
