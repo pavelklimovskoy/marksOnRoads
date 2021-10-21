de3cc2c7bb25d6e503805fde8fe5058425315d9e
DmitryDichenskov <86311960+DmitryDichenskov@users.noreply.github.com>
2021-10-21 15:56:23 +0300

    21.10.21

---
+++ html/Elkin/js/script.js
@@ -0,0 +1,17 @@
+function onLoadButton() {
+    alert("Кнопка загрузки")
+  }
+  function onSaveButton() {
+    alert("Кнопка сохранения")
+  }
+  function onAgentButton() {
+    //alert("Кнопка добаввления агента")
+    _agentButton = true;
+  }
+  function onRoadButton() {
+    alert("Кнопка добавления дороги")
+    _roadButton = true;
+  }
+  function onExecuteButton() {
+    alert("Кнопка вывода файла")
+  }
